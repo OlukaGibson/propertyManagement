d68666469592a8e7c5a49a8296ceff26c4916395
gibson oluka <gibsonoluka7@gmail.com>
2025-01-01 16:47:05 +0300

    init deployment

--- propertymngt/__init__.py
+++ propertymngt/__init__.py
@@ -1,13 +1,21 @@
 import os
 from flask import Flask
-from .extentions import db
+from .extentions import db,migrate
 from .routes import propertymngt
 from flask_cors import CORS
 
+'''
+{% .... %}
+{{ .... }}
+{# .... #}
+'''
+
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = os
+    # app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+    # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = os
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:///default.db')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
     migrate.init_app(app, db)
@@ -15,5 +23,7 @@ def create_app():
     app.register_blueprint(propertymngt)
 
     CORS(app, origins=["http://localhost:3000"])
+
+    app.debug = True
 
     return app
