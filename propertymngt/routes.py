369bf1c5993261970d390a4a2098ef7a06694424
OlukaGibson <gibsonoluka7@gmail.com>
2025-01-06 11:40:44 +0300

    Outsource video done

--- propertymngt/routes.py
+++ propertymngt/routes.py
@@ -1,53 +1,43 @@
-from flask import Blueprint, redirect, url_for, render_template, request, send_file, jsonify, Response
-import os
-from .extentions import db
-from .models import Users, Guests
-# from google.cloud import storage
-import io
-import json
-from dotenv import load_dotenv
-# from intelhex import IntelHex
-from datetime import datetime
+from flask import Blueprint, render_template, request, Response
 import cv2
-
-load_dotenv()
+import numpy as np
+from queue import Queue
 
 propertymngt = Blueprint('propertymngt', __name__)
 
-camera = cv2.VideoCapture(1)
+# Queue to hold frames received from the camera
+frame_queue = Queue(maxsize=10)
 
 def gen_frames():
+    """Generate frames for video streaming."""
     while True:
-        success, frame = camera.read()
-        if not success:
-            print('Camera not found!')
-            break
-        else:
-            # detector = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-            # eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
-            # faces = detector.detectMultiScale(frame, 1.1, 7)
-            # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            # # rectangle of faces
-            # for (x, y, w, h) in faces:
-            #     cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
-            #     roi_gray = gray[y:y+h, x:x+w]
-            #     roi_color = frame[y:y+h, x:x+w]
-            #     eyes = eye_cascade.detectMultiScale(roi_gray)
-            #     for (ex, ey, ew, eh) in eyes:
-            #         cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
-
-            ret, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()
+        if not frame_queue.empty():
+            frame = frame_queue.get()
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        else:
+            # If no frames are available, send a placeholder frame (optional)
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + b'\r\n')
 
- 
 @propertymngt.route('/')
 def index():
-    print('Device storage is full!')
+    """Serve the index page."""
     return render_template('index.html')
-    # return {'message': 'Device storage is full!'}
 
 @propertymngt.route('/video')
 def video():
+    """Stream video frames to clients."""
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@propertymngt.route('/video_stream', methods=['POST'])
+def video_stream():
+    """Receive video frames from the camera."""
+    if 'frame' in request.files:
+        frame = request.files['frame'].read()
+        # Add the frame to the queue
+        if frame_queue.full():
+            frame_queue.get()  # Remove the oldest frame
+        frame_queue.put(frame)
+        return "Frame received", 200
+    return "No frame received", 400
